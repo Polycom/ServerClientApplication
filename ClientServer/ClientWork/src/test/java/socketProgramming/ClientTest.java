07c8be5ca764cbfee99b8312f3be534031789042
Madhavi <Madhavi.Giddaluri@polycom.com>
2015-03-02 14:31:43 +0530

    second commit

---
+++ ClientServer/ClientWork/src/test/java/socketProgramming/ClientTest.java
@@ -0,0 +1,16 @@
+package socketProgramming;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClientTest {
+
+	@Test
+	public void test() 
+	{
+		Client client=new Client();
+		client.m
+	}
+
+}
